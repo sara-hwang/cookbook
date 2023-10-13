89b0122b0a1602b10d0baa79a3c6428c0292766e
Sara Hwang <shw9925@gmail.com>
2023-10-12 21:12:27 -0700

    Add tabs for add and view recipes

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,63 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Field, Form, Formik } from "formik";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+import { Box, Paper, Grid, Button, Tab, Tabs, Typography } from "@mui/material";
 
 import axios from "axios";
 import TextField from "@mui/material/TextField";
+import AddRecipe from "./AddRecipe";
 
-const App = ({}) => {
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div>
-      <Formik
-        initialValues={{}}
-        onSubmit={(data, { setSubmitting }) => {
-          setSubmitting(true);
-          console.log("submtting");
-          axios
-            .post("http://localhost:3001/add", data)
-            .then((result) => console.log(result))
-            .catch((error) => console.log(error));
-          setSubmitting(false);
-        }}
-      >
-        {({ values, errors, isSubmitting }) => (
-          <Form>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={8}>
-                  <Field
-                    placeholder="Title"
-                    name="title"
-                    type="input"
-                    as={TextField}
-                  />
-                  <Button disabled={isSubmitting} type="submit">
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </Box>
-          </Form>
-        )}
-      </Formik>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
-};
+}
 
-export default App;
+export default function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Add Recipe" />
+          <Tab label="View Recipes" />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <AddRecipe />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        todo
+      </TabPanel>
+    </Box>
+  );
+}
