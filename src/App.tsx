fd707548d3216f8eed2ba90b6978d20b6bc65300
Sara Hwang <sha173@sfu.ca>
2023-10-12 20:41:35 -0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Field, Form, Formik } from "formik";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 
-function App() {
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+
+const App = ({}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik
+        initialValues={{}}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          console.log("submtting");
+          axios
+            .post("http://localhost:3001/add", data)
+            .then((result) => console.log(result))
+            .catch((error) => console.log(error));
+          setSubmitting(false);
+        }}
+      >
+        {({ values, errors, isSubmitting }) => (
+          <Form>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={8}>
+                  <Field
+                    placeholder="Title"
+                    name="title"
+                    type="input"
+                    as={TextField}
+                  />
+                  <Button disabled={isSubmitting} type="submit">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
