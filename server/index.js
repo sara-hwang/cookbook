253219f2c6a943931600ed48c0613a5b4cf3b41e
Sara Hwang <shw9925@gmail.com>
2023-10-14 12:17:31 -0700

    Update connection string

--- server/index.js
+++ server/index.js
@@ -2,28 +2,34 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const RecipeModel = require("./models/recipes");
-const { error } = require("console");
 
+require("dotenv").config({ path: "../.env" });
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb+srv://sara:sara123abc@cookbook.efjxjit.mongodb.net/cookbook"
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cookbook.efjxjit.mongodb.net/cookbook`
 );
 
-app.get("/get", (req, res) => {
-  RecipeModel.find()
-    .then((result) => res.json(result))
-    .catch((error) => res.json(error));
+app.get("/get", async (req, res) => {
+  try {
+    let response = await RecipeModel.find();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.post("/add", (req, res) => {
   const obj = req.body;
   console.log(obj);
-  RecipeModel.create(obj)
-    .then((result) => res.json(result))
-    .catch((error) => console.log(error));
+  try {
+    let response = RecipeModel.create(obj);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(3001, () => {
