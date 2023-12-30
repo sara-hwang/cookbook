1d23200ea68bfb6ed274ed74965065bbfe8fc477
Sara Hwang <shw9925@gmail.com>
2023-12-29 22:46:44 -0800

    Change theme font

--- src/theme.ts
+++ src/theme.ts
@@ -4,23 +4,18 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      // light: "#f4bfb3",
       main: lightGreen[900],
-      // dark: "#ae2202",
-      // contrastText: "#fff",
     },
     secondary: {
-      // light: "#FFE6D7",
       main: lightGreen[200],
-      // dark: "#e6cfc2",
-      // contrastText: "#000",
     },
-    // action: {
-    // selectedOpacity: 0.3,
-    // selected: "#562d2e",
-    // hover: "#306B00",
-    // focus: "#d92a02",
-    // },
+  },
+  typography: {
+    fontFamily: `"Rubik", "Roboto", "Helvetica", "Arial", sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
   },
 });
 
