1150fee10f3c697d8da6d64b8e3c8b1c2a07cbc9
Sara Hwang <shw9925@gmail.com>
2024-03-06 23:39:10 -0800

    Update random button

--- src/components/recipes/view/all/RandomButton.tsx
+++ src/components/recipes/view/all/RandomButton.tsx
@@ -1,16 +1,15 @@
 import { Casino } from "@mui/icons-material";
 import { Button } from "@mui/material";
-import { useState } from "react";
 import { useAppDispatch } from "../../../../redux/hooks";
 import { pushTab } from "../../../../redux/tabsList";
 import { Recipe } from "../../../../utils/types";
+import theme from "../../../../utils/theme";
 
 interface RandomButtonProps {
   recipes: Recipe[];
 }
 
 const RandomButton = ({ recipes }: RandomButtonProps) => {
-  const [randomDisabled, setRandomDisabled] = useState(false);
   const dispatch = useAppDispatch();
 
   return (
@@ -18,27 +17,22 @@ const RandomButton = ({ recipes }: RandomButtonProps) => {
       size="small"
       component="span"
       disableRipple
-      variant="outlined"
-      color="info"
-      className={randomDisabled ? "random-disabled" : undefined}
-      // add a timeout otherwise spam-clicking leads to bugs
-      onClick={
-        randomDisabled
-          ? undefined
-          : async () => {
-              setRandomDisabled(true);
-              setTimeout(() => {
-                setRandomDisabled(false);
-              }, 1000);
-              const randomIndex = Math.floor(Math.random() * recipes.length);
-              dispatch(
-                pushTab({
-                  label: recipes[randomIndex].title,
-                  link: `/view/${recipes[randomIndex].key}`,
-                })
-              );
-            }
-      }
+      variant="contained"
+      color="secondary"
+      sx={{
+        "&:hover": {
+          backgroundColor: theme.palette.secondary.main,
+        },
+      }}
+      onClick={() => {
+        const randomIndex = Math.floor(Math.random() * recipes.length);
+        dispatch(
+          pushTab({
+            label: recipes[randomIndex].title,
+            link: `/view/${recipes[randomIndex].key}`,
+          })
+        );
+      }}
     >
       <Casino fontSize="small" />
       &nbsp;Random
