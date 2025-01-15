7cf5ffb2dc477e313edb4cb4b05ab75ea556a049
Sara Hwang <shw9925@gmail.com>
2025-01-14 18:40:20 -0800

    add add meal button

--- src/components/log/MealLog.tsx
+++ src/components/log/MealLog.tsx
@@ -1,7 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
+import { useState } from "react";
 
 const MealLog = () => {
-  return <Box sx={{ padding: "24px" }}></Box>;
+  const [dialogOpen, setDialogOpen] = useState(false)
+  return (
+    <Box sx={{ padding: "24px" }}>
+      <Grid container spacing={2}>
+        <Grid item container justifyContent="flex-end" spacing={1}>
+          <Grid item>
+            <Button
+              variant="contained"
+              onClick={() => setDialogOpen(true)}
+            >
+              Add Meal
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default MealLog;
