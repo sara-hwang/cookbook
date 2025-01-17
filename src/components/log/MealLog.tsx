2f57ae78e803730c0607a0e2e3718917d0c243df
Sara Hwang <shw9925@gmail.com>
2025-01-16 18:03:34 -0800

    add dialog to meal log page

--- src/components/log/MealLog.tsx
+++ src/components/log/MealLog.tsx
@@ -1,23 +1,27 @@
 import { Box, Button, Grid } from "@mui/material";
 import { useState } from "react";
+import AddMealDialog from "./AddMealDialog";
 
 const MealLog = () => {
   const [dialogOpen, setDialogOpen] = useState(false)
+
   return (
-    <Box sx={{ padding: "24px" }}>
-      <Grid container spacing={2}>
-        <Grid item container justifyContent="flex-end" spacing={1}>
-          <Grid item>
-            <Button
-              variant="contained"
-              onClick={() => setDialogOpen(true)}
-            >
-              Add Meal
-            </Button>
+    <>
+      {dialogOpen && (
+        <AddMealDialog dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} />
+      )}
+      <Box sx={{ padding: "24px" }}>
+        <Grid container spacing={2}>
+          <Grid item container justifyContent="flex-end" spacing={1}>
+            <Grid item>
+              <Button variant="contained" onClick={() => setDialogOpen(true)}>
+                Add Meal
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 
