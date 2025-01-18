1d6c8f256e894580d2a046998f336a51f7b3dde3
Sara Hwang <shw9925@gmail.com>
2025-01-17 20:05:28 -0800

    add fields to add meal dialog

--- src/components/log/AddMealDialog.tsx
+++ src/components/log/AddMealDialog.tsx
@@ -45,6 +45,87 @@ export const AddMealDialog = ({
           <CloseIcon />
         </IconButton>
       </DialogTitle>
+      <DialogContent>
+        <Formik
+          validateOnChange={false}
+          validateOnBlur={false}
+          initialValues={{ name: "", password: "", portions: 1 }}
+          enableReinitialize={true}
+          validationSchema={validationSchema}
+          onSubmit={async (data: {
+            name: string;
+            password: string;
+            portions: number;
+          }) => {
+            return true;
+          }}
+        >
+          {({ errors, isValid, isSubmitting, dirty }) => (
+            <Form>
+              <Grid
+                container
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={2}
+              >
+                <Grid item sx={{ marginTop: "10px", width: "100%" }}>
+                  <Field
+                    name="meal name"
+                    type="input"
+                    as={Autocomplete}
+                    label="Meal Name"
+                    size="small"
+                    error={errors.name !== undefined}
+                    helperText={errors.name}
+                    sx={{ width: "100%" }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    name="recipe"
+                    type={"input"}
+                    as={TextField}
+                    label="food"
+                    size="small"
+                    error={errors.password !== undefined}
+                    helperText={errors.password}
+                  />
+                </Grid>
+                <Grid item sx={{ marginTop: "10px", width: "100%" }}>
+                  <Field
+                    name="portions"
+                    type="number"
+                    as={TextField}
+                    label="Portions"
+                    InputProps={{
+                      inputProps: { min: "0", step: "any" },
+                    }}
+                    size="small"
+                    error={errors.portions !== undefined}
+                    helperText={errors.portions}
+                    sx={{ width: "100%" }}
+                  />
+                </Grid>
+                {errorMessage && (
+                  <Grid item xs={12} sx={{ color: "red" }}>
+                    {errorMessage}
+                  </Grid>
+                )}
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    disabled={!dirty || !isValid || isSubmitting}
+                    type="submit"
+                  >
+                    Save
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </DialogContent>
     </Dialog>
   );
 };
